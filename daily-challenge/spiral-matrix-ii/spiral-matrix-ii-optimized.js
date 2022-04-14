015764c959b690a5832bdb410e78251d24c0f1df
Victor Fajardo <fajardo.de.leon@gmail.com>
2022-04-13 23:09:16 -0400

    adding daily challenge

---
+++ daily-challenge/spiral-matrix-ii/spiral-matrix-ii-optimized.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+var generateMatrix = function (n) {
+  const matrix = new Array(n).fill(0).map(() => new Array(n).fill(0))
+  const directions = [
+    [0, 1],
+    [1, 0],
+    [0, -1],
+    [-1, 0]
+  ]
+  let counter = 1
+  let dir = 0
+  let row = 0
+  let col = 0
+
+  while (counter <= n * n) {
+    matrix[row][col] = counter
+    counter++
+
+    const nextRow = row + directions[dir][0]
+    const nextCol = col + directions[dir][1]
+
+    if (nextRow < 0 || nextRow > n - 1 || nextCol < 0 || nextCol > n - 1 || matrix[nextRow][nextCol] !== 0) dir = (dir + 1) % 4
+
+    row += directions[dir][0]
+    col += directions[dir][1]
+  }
+
+  return matrix
+};
---
+++ daily-challenge/spiral-matrix-ii/spiral-matrix-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number} n
+ * @return {number[][]}
+ */
+
+const generateMatrix = (n) => {
+  const matrix = new Array(n).fill(0).map(() => new Array(n))
+  let layer = 0
+  let counter = 1
+
+  while (counter <= n * n) {
+    for (let i = layer; i < n - layer; i++) {
+      matrix[layer][i] = counter
+      counter++
+    }
+
+    for (let i = layer + 1; i < n - layer; i++) {
+      matrix[i][n - layer - 1] = counter
+      counter++
+    }
+
+    for (let i = layer + 1; i < n - layer; i++) {
+      matrix[n - layer - 1][n - 1 - i] = counter
+      counter++
+    }
+
+    for (let i = layer + 1; i < n - layer - 1; i++) {
+      matrix[n - 1 - i][layer] = counter
+      counter++
+    }
+    layer++
+  }
+
+  return matrix
+};
